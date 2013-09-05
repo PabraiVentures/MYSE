d2994f214b7a9746c700cdc6fd1f34a0a84d54f5
Nathan Pabrai <nathanpabrai@gmail.com>
2013-09-05 18:51:41 -0400

    Revert 94a55bb..23e0ee3
    This rolls back to commit 94a55bbe392d5c7632ddb8f6c287d096edca5c5a.

--- BayTrade/BT_TradeViewController.m
+++ BayTrade/BT_TradeViewController.m
@@ -113,8 +113,8 @@
     if(needsUpdate)
     {
         /***Updates display HERE now***/
-       [self updateBuyPower];
-       [self updateValue];
+        [self updateBuyPower];
+        [self updateValue];
     }
         
     } onFailure:^(NSError *error) {
@@ -343,9 +343,6 @@
     }
     return amtAndPrc;
 }
-
-
-
 
 
 /**
