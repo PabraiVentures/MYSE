d262d31ba54242ce09fb193f08abcdf839840444
manbropig <jamie@Jamies-MacBook-Pro-2.local>
2013-09-10 22:23:10 -0700

    Trying to add AutoComplete
    Seems to do everything necessary in the backend but no drop down list
    appears. Trying to understand why.

--- BayTrade/BT_TradeViewController.h
+++ BayTrade/BT_TradeViewController.h
@@ -10,7 +10,7 @@
 #import "Cache.h"
 #import <FacebookSDK/FacebookSDK.h>
 
-@interface BT_TradeViewController : UIViewController <FBLoginViewDelegate, UIAlertViewDelegate> {
+@interface BT_TradeViewController : UIViewController <FBLoginViewDelegate, UIAlertViewDelegate, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate> {
     double reservedBuyPrice;
     double reservedSalePrice;
     NSString *saleSymbol;
@@ -28,10 +28,15 @@
 @property (weak, nonatomic) IBOutlet FBProfilePictureView *profilePic;
 @property (weak, nonatomic) IBOutlet UILabel *investorName;
 
+@property (nonatomic, retain) NSMutableArray *autocompleteSuggestions;
+@property (nonatomic, retain) NSMutableArray *autocompleteSymbols;
+@property (nonatomic, retain) UITableView *autocompleteTableView;
+
 - (IBAction)buyButtonClicked:(id)sender;
 - (IBAction)sellButtonClicked:(id)sender;
 - (IBAction)logoutButtonClicked:(id)sender;
 - (NSMutableArray *) accountForPrevOwnedStock: (NSString *) symbol andInt: (int) amount andPrice: (double) price;
+- (void)searchAutocompleteEntriesWithSubstring:(NSString *)substring;
 - (void) setCoreModel;
 
 @end
