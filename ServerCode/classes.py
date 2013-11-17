b0e94d4d8ef37334c6210c397d60ac719ab3f724
Nathan Pabrai <nathanpabrai@gmail.com>
2013-11-17 02:53:09 -0500

    servercode

---
+++ ServerCode/classes.py
@@ -0,0 +1,84 @@
+#HELLO: to install: on command line do easy_install oauth, then easy_install urllib2.
+
+import oauth.oauth as oauth
+import httplib
+import json
+import sys
+import urllib2
+import time
+class BaseClient:
+	
+	def __init__(self, baseURL, key, secret):
+		self.url = baseURL
+		self.connection = httplib.HTTPConnection(baseURL)
+		self.consumer = oauth.OAuthConsumer(key, secret)
+	def _execute(self,m, httpmethod, path, body):
+		request = oauth.OAuthRequest.from_consumer_and_token(self.consumer, http_method=httpmethod, http_url="http://" + self.url + "/" + path)
+		request.sign_request(oauth.OAuthSignatureMethod_HMAC_SHA1(), self.consumer, None)
+		headers = request.to_header()
+		headers["Accept"] = "application/vnd.stackmob+json; version=0"
+		headers["X-StackMob-API-Key"]="ef598654-95fb-4ecd-8f13-9309f2fcad0f"
+		if (m is 0) :
+			headers["X-Stackmob-Expand"]="1"
+		headers["Content-Type"] = "application/json"
+		#headers["Version"]="0"
+		self.connection.set_debuglevel(1)
+		bodyString = ""
+		if(body != None):
+			bodyString = json.dumps(body)
+		self.connection.request(request.http_method, "/"+path, body=bodyString, headers=headers)
+		return self.connection.getresponse()
+
+	def get(self, path):
+		self._execute("GET", path, None)
+	def post(self, path, body):
+		self._execute("POST", path, body)
+	def put(self, path, body):
+		self._execute("PUT", path, body)
+	def delete(self, path):
+		self._execute("DELETE", path, None)
+
+class APIClient(BaseClient):
+	def __init__(self, key, secret):
+		super.__init__("api.mob1.stackmob.com", key, secret)
+
+class PushAPIClient(BaseClient):
+	def __init__(self, key, secret):
+		super.__init__("push.mob1.stackmob.com", key, secret)
+		
+class Pyql:
+
+	def lookup(self,symbols):
+		yql = "select * from yahoo.finance.quotes where symbol in (" \
+						+ '\'' \
+						+ '\',\''.join( symbols ) \
+						+ '\'' \
+						+ ")"
+					
+		url = "http://query.yahooapis.com/v1/public/yql?q=" \
+				+ urllib2.quote( yql ) \
+				+ "&format=json&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback="
+	
+		try: 
+			result = urllib2.urlopen(url)
+		except urllib2.HTTPError, e:        
+			print ("HTTP error: ", e.code)        
+		except urllib2.URLError, e:
+			print ("Network error: ", e.reason)
+			
+		pythonQuotes = []
+		jsonQuote={}
+		try:	   
+			data = json.loads( result.read() )
+			jsonQuotes = data['query']['results']['quote']
+
+			#raise
+			# To make sure the function returns a list
+			if type( jsonQuotes ) == type ( dict() ):
+				pythonQuotes.append( jsonQuotes )
+			else:
+				pythonQuotes = jsonQuotes
+		except:
+			i=2	
+		return pythonQuotes
+
---
+++ ServerCode/fillorders.py
@@ -0,0 +1,65 @@
+#HELLO: to install: on command line do easy_install oauth, then easy_install urllib2.
+
+import oauth.oauth as oauth
+import httplib
+import json
+import sys
+import urllib2
+import time
+import classes as cl
+		
+def executeOrder(ticker,portfolio,price,amount,type,stockorder_id,client):
+	print ticker+" "+portfolio+" "+str(price)+" "+str(amount)+" "+str(type)+" "+stockorder_id
+	str1="coreportfolio/"+portfolio
+	port= json.loads(client._execute(0,"GET",str1,None).read())
+	foundstock=0
+	for i in port['stocks']:
+		if ticker == i['symbol']:
+			foundstock=1
+			if type<3:
+				body={"amount":(amount+i['amount'])}
+			if type>=3:
+				body={"amount":(i['amount']-amount)}
+			str2="corestock/"+i['corestock_id']
+			client._execute(1,"PUT",str2,body).read()
+
+		
+			break
+	if foundstock==0:
+		body={"amount":amount,"portfolio":port['coreportfolio_id'],"symbol":ticker,"sm_owner":port['sm_owner']}
+		str2="corestock"
+		client._execute(1,"POST",str2,body).read()
+	if type<3:
+		body={"totalcashvalue":port['totalcashvalue']-price*amount}
+	else:
+		body={"totalcashvalue":port['totalcashvalue']+price*amount}
+	str2="coreportfolio/"+port['coreportfolio_id']
+	client._execute(1,"PUT",str2,body).read()
+	
+	client._execute(1,"DELETE","stockorder/"+stockorder_id,None).read()
+		 
+while 2>0:
+	try:
+		client=cl.BaseClient("api.mob1.stackmob.com","ef598654-95fb-4ecd-8f13-9309f2fcad0f", "9ac9ecaa-21eb-4ef2-8ddc-10ce40ca67e4")
+		w=client._execute(1,"GET","stockorder",None)
+		u= w.read()
+		orders=json.loads(u)
+		print "--------------"
+		print orders
+		print ""
+		for i in orders:
+			print i['symbol']
+			if i['tradetype']==0 or i['tradetype']==3:
+				executeOrder(i['symbol'],i['portfolio'],i['price'],i['quantity'],i['tradetype'],i['stockorder_id'],client)
+			else:
+				currprice=float(cl.Pyql().lookup([i['symbol']])[0]['LastTradePriceOnly'])
+				if (i['tradetype']==1 or i['tradetype']==5) and (currprice  <= i['price']):
+					executeOrder(i['symbol'],i['portfolio'],i['price'],i['quantity'],i['tradetype'],i['stockorder_id'],client)
+			
+				if (i['tradetype']==2 or i['tradetype']==4) and (currprice  >= i['price']):
+					executeOrder(i['symbol'],i['portfolio'],i['price'],i['quantity'],i['tradetype'],i['stockorder_id'],client)
+			
+	except:
+		n=2
+	#know that type is not market so need to check current price
+
